30356308ab2912b41d66e30c8aea176f871dc14f
Bünyamin Arıcı <45267994+kralakrep65@users.noreply.github.com>
2025-01-08 21:57:22 +0300

    remove unused import

--- lib/screens/splahs_screen/splash_screen.dart
+++ lib/screens/splahs_screen/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:panko_birlik/utils/app_colors.dart';
 import 'package:panko_birlik/utils/app_text_styles.dart';
 
 class SplashScreen extends StatefulWidget {
